739f3babf9fc3dfef7d9cddf6f1d91b316a927f7
Douglas Schussler <schussler855@gmail.com>
2023-05-14 10:24:59 -0300

    Update db.js
    Removendo o url do meu banco de dados

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV == "production") {
   module.exports = {
     mongoURI:
-      "mongodb+srv://schussler855:QWzXse5rWYSxTHkN@cluster0.tzjlkuv.mongodb.net/",
+      "URL_BD_LINK-AQUI",
   };
 } else {
   module.exports = { mongoURI: "mongodb://127.0.0.1/blogapp" };
